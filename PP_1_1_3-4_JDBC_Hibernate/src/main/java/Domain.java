2ee5ebe53b65637f223f19f1f2ab77a63e1f9074
adrian-0316 <112898743+adrian-0316@users.noreply.github.com>
2023-03-05 00:44:52 +0300

    Add files via upload

---
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/Domain.java
@@ -0,0 +1,9 @@
+import jm.task.core.jdbc.Main;
+import jm.task.core.jdbc.util.Util;
+
+public class Domain {
+    public static void main(String[] args) {
+        Util util = new Util();
+        util.getConnection();
+    }
+}
